6efb3d4f11c1b68bd76253e32a6aa5335f492df1
SDSKNIGHT <s2024004@ed.ac.uk>
2022-10-10 00:16:45 +0100

    I hate programming

--- src/main/java/uk/ac/ed/inf/LngLat.java
+++ src/main/java/uk/ac/ed/inf/LngLat.java
@@ -1,8 +1,11 @@
 package uk.ac.ed.inf;
 
 public record LngLat(double lng, double lat){
+    public Boolean inCentralArea(){
+        
+        return null;
+    }
 
 
 
-
-};
+}
---
+++ src/main/java/uk/ac/ed/inf/Singleton.java
@@ -0,0 +1,23 @@
+package uk.ac.ed.inf;
+
+import java.util.ArrayList;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Singleton {
+
+    private static Singleton instance= null;
+
+    public static Singleton getInstance(){
+        if(instance==null){
+            Singleton instance = new Singleton();
+        }
+        return instance;
+    }
+
+
+
+
+}
