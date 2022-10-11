a90e7a0fb15c30e433300c36ebcff6c68fc293c4
SDSKNIGHT <s2024004@ed.ac.uk>
2022-10-12 00:33:26 +0100

    uppudatto

--- src/main/java/uk/ac/ed/inf/App.java
+++ src/main/java/uk/ac/ed/inf/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LngLat doug = new LngLat(55.55,55.55);
+        System.out.println();
     }
 }
--- src/main/java/uk/ac/ed/inf/LngLat.java
+++ src/main/java/uk/ac/ed/inf/LngLat.java
@@ -19,6 +19,33 @@ public record LngLat(double lng, double lat){
         double distance = Math.sqrt(Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2));
         return distance;
     }
+    public boolean closeTo (LngLat secPos) {
+        if (secPos == null){
+            throw new NullPointerException("The input cannot be null");
+        }
+
+        if (distanceTo (secPos) < 0.00015) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public LngLat nextPosition ( enum angle){
+
+
+
+        if (angle == null){
+            return this;
+            //DO A CONVERSION OF THE ENUM DIRECTIONS INTO VALUES
+        } else {
+            double rads = //rads calculation
+            double newLng = this.lng + Math.cos (rads) * 0.00015;
+            double newLat = this.lat + Math.sin (rads) * 0.00015;
+
+            LngLat newPos = new LngLat (newLng, newLat);
+            return newPos;
+        }
+    }
 
 
 
